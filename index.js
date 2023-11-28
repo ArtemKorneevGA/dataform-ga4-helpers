e6becc42cdabe1c33256a2e05bd81955076e4542
Artem Korneev <dev.dandp@gmail.com>
2023-11-28 07:43:58 +0100

    feat: added functions for sessions and unit tests

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ const getSqlEventParam = (
   paramName,
   paramType = "string",
   columnName = false
-) => getUnnestParam(paramName, paramType, columnName);
+) => getSqlUnnestParam(paramName, paramType, columnName);
 
 const getSqlUserProperty = (
   paramName,
   paramType = "string",
   columnName = false
-) => getUnnestParam(paramName, paramType, columnName, "user_properties");
+) => getSqlUnnestParam(paramName, paramType, columnName, "user_properties");
 
-const getUnnestParam = (
+const getSqlUnnestParam = (
   paramName,
   paramType = "string",
   resultColumnName = false,
   unnestColumnName = "event_params"
 ) => {
-  let paramTypeName = "";
-  switch (paramType) {
+  let paramTypeName;
+  const alias =
+    resultColumnName === null
+      ? ""
+      : `AS ${resultColumnName ? resultColumnName : paramName} `;
+  if (paramType.toLowerCase() === "coalesce") {
+    return `(SELECT COALESCE(ep.value.string_value, SAFE_CAST(ep.value.int_value AS STRING), SAFE_CAST(ep.value.double_value AS STRING), SAFE_CAST(ep.value.float_value AS STRING)) FROM UNNEST(${unnestColumnName}) ep WHERE ep.key = '${paramName}' LIMIT 1) ${alias}`;
+  }
+  switch (paramType.toLowerCase()) {
     case "string":
       paramTypeName = "string_value";
       break;
@@ -33,10 +40,72 @@ const getUnnestParam = (
     default:
       throw "eventType is not valid";
   }
-  return `(SELECT ep.value.${paramTypeName} AS ${paramName} FROM UNNEST(${unnestColumnName}) ep WHERE ep.key = '${paramName}') AS ${
-    resultColumnName ? resultColumnName : paramName
-  }`;
+  return `(SELECT ep.value.${paramTypeName} FROM UNNEST(${unnestColumnName}) ep WHERE ep.key = '${paramName}' LIMIT 1) ${alias}`;
 };
+
+
+const getSqlEventParams = (eventParams) => {
+  const sql = eventParams.map((eventParam) =>
+    getSqlEventParam(eventParam.name, eventParam.type, eventParam.columnName)
+  );
+  return eventParams.length > 0 ? sql.join(", ") : "";
+};
+
+const getSqlUserProperties = (userProperties) => {
+  const sql = userProperties.map((userProperty) =>
+    getSqlUserProperty(
+      userProperty.name,
+      userProperty.type,
+      userProperty.columnName
+    )
+  );
+  return userProperties.length > 0 ? sql.join(", ") : "";
+};
+
+
+const getSqlGetFirstNotNullValue = (
+  paramName,
+  columnName = false,
+  orderBy = "event_timestamp"
+) => {
+  const alias =
+    columnName === null ? "" : `AS ${columnName ? columnName : paramName} `;
+  return `ARRAY_AGG(${paramName} IGNORE NULLS ORDER BY ${orderBy} LIMIT 1)[SAFE_OFFSET(0)] ${alias}`;
+};
+
+const getSqlGetFirstNotNullValues = (columns) => {
+  const sql = columns.map((column) =>
+    getSqlGetFirstNotNullValue(
+      column.columnName ? column.columnName : column.name
+    )
+  );
+  return columns.length > 0 ? sql.join(",") : "";
+};
+
+
+const getSqlColumns = (params) => {
+  const sql = params.map(
+    (param) =>
+      `${param.name} as ${param.columnName ? param.columnName : param.name}`
+  );
+  return params.length > 0 ? sql.join(", ") : "";
+};
+
+
+const getSqlQueryParameter = (url, param) => {
+  const sql = `REGEXP_EXTRACT(${url}, r'(?i).*[?&#]${param.name.toLowerCase()}=([^&#\?]*)') as ${
+    param.columnName ? param.columnName : param.name
+  }`;
+  return sql;
+};
+
+
+const getSqlQueryParameters = (url, params) => {
+  const sql = params.map((param) => getSqlQueryParameter(url, param));
+  return params.length > 0 ? sql.join(", ") : "";
+};
+
+
 
 const getDateFromTableName = (tblName) => {
   return tblName.substring(7);
@@ -63,13 +132,68 @@ const getSqlSessionId = () => {
 const getSqlDate = (timezone = "Europe/London") =>
   `DATE(TIMESTAMP_MICROS(event_timestamp), "${timezone}") as date`;
 
+  function isStringInteger(str) {
+    const num = Number(str);
+    return Number.isInteger(num);
+  }
+  
+const getSqlSelectFromRow = (config) => {
+  return Object.entries(config)
+    .map(([key, value]) => {
+      if (typeof value === "number") {
+        return `${value} AS ${key}`;
+      } else if (key === "date") {
+        return `DATE '${value}' AS ${key}`;
+      } else if (key === "session_start") {
+        return `TIMESTAMP '${value}' AS ${key}`;
+      } else if (key === "session_end") {
+        return `TIMESTAMP '${value}' AS ${key}`;
+      } else if (typeof value === "string") {
+        if (key === "int_value") return `${parseInt(value)} AS ${key}`;
+        if (key.indexOf("timestamp") > -1)
+          return `${parseInt(value)} AS ${key}`;
+        if (key === "float_value" || key === "double_value")
+          return `${parseFloat(value)} AS ${key}`;
+        return `'${value}' AS ${key}`;
+      } else if (value === null) {
+        return `${value} AS ${key}`;
+      } else if (value instanceof Array) {
+        return `[${getSelectStatement(value)}] AS ${key}`;
+      } else {
+        if (isStringInteger(key)) return `STRUCT(${getSelectStatement(value)})`;
+        else return `STRUCT(${getSelectStatement(value)}) AS ${key}`;
+      }
+    })
+    .join(", ");
+};
+
+const getSqlUnionAllFromRows = (rows) => {
+  try {
+    const selectStatements = rows
+      .map((data) => "SELECT " + getSqlSelectFromRow(data))
+      .join("\nUNION ALL\n ");
+    return selectStatements;
+  } catch (err) {
+    console.error("Error reading or parsing the file", err);
+  }
+};
+  
+  
 module.exports = {
   getDateFromTableName,
   getFormattedDateFromTableName,
   getSqlEventParam,
+  getSqlEventParams,
   getSqlUserProperty,
+  getSqlUserProperties,
   getSqlList,
   getSqlEventId,
   getSqlSessionId,
   getSqlDate,
+  getSqlGetFirstNotNullValue,
+  getSqlGetFirstNotNullValues,
+  getSqlColumns,
+  getSqlQueryParameter,
+  getSqlQueryParameters,
+  getSqlUnionAllFromRows,
 };
